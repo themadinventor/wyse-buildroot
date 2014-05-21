88559931431e179e55f2a72189ba9f6070358205
Fredrik Ahlberg <fredrik@z80.se>
2014-05-21 15:43:45 +0200

    add minimal syslinux to target

--- boot/syslinux/syslinux.mk
+++ boot/syslinux/syslinux.mk
@@ -11,8 +11,8 @@ SYSLINUX_SITE    = $(BR2_KERNEL_MIRROR)/linux/utils/boot/syslinux/4.xx/
 SYSLINUX_LICENSE = GPLv2+
 SYSLINUX_LICENSE_FILES = COPYING
 
-SYSLINUX_INSTALL_TARGET = NO
-SYSLINUX_INSTALL_IMAGES = YES
+#SYSLINUX_INSTALL_TARGET = YES
+#SYSLINUX_INSTALL_IMAGES = NO
 
 SYSLINUX_DEPENDENCIES = host-nasm host-util-linux
 
@@ -20,25 +20,29 @@ SYSLINUX_DEPENDENCIES = host-nasm host-util-linux
 # and the internal zlib should take precedence so -I shouldn't
 # be used.
 define SYSLINUX_BUILD_CMDS
-	$(TARGET_MAKE_ENV) $(MAKE) CC="$(HOSTCC) -idirafter $(HOST_DIR)/usr/include $(HOST_LDFLAGS)" AR="$(HOSTAR)" -C $(@D)
+	$(TARGET_MAKE_ENV) $(MAKE) CC="$(TARGET_CC) $(TARGET_LDFLAGS)" -C $(@D)
 endef
 
-SYSLINUX_IMAGES-$(BR2_TARGET_SYSLINUX_ISOLINUX) += isolinux.bin
-SYSLINUX_IMAGES-$(BR2_TARGET_SYSLINUX_PXELINUX) += pxelinux.bin
+#SYSLINUX_IMAGES-$(BR2_TARGET_SYSLINUX_ISOLINUX) += isolinux.bin
+#SYSLINUX_IMAGES-$(BR2_TARGET_SYSLINUX_PXELINUX) += pxelinux.bin
 
-define SYSLINUX_INSTALL_IMAGES_CMDS
-	for i in $(SYSLINUX_IMAGES-y); do \
-		$(INSTALL) -D -m 0755 $(@D)/core/$$i $(BINARIES_DIR)/$$i; \
-	done
-endef
+#define SYSLINUX_INSTALL_IMAGES_CMDS
+#	for i in $(SYSLINUX_IMAGES-y); do \
+#		$(INSTALL) -D -m 0755 $(@D)/core/$$i $(BINARIES_DIR)/$$i; \
+#	done
+#endef
 
 
-define HOST_SYSLINUX_BUILD_CMDS
-	$(HOST_MAKE_ENV) $(MAKE) -C $(@D)
-endef
+#define HOST_SYSLINUX_BUILD_CMDS
+#	$(HOST_MAKE_ENV) $(MAKE) -C $(@D)
+#endef
 
-define HOST_SYSLINUX_INSTALL_CMDS
-	$(HOST_MAKE_ENV) $(MAKE) -C $(@D) INSTALLROOT=$(HOST_DIR) install
+#define HOST_SYSLINUX_INSTALL_CMDS
+#	$(HOST_MAKE_ENV) $(MAKE) -C $(@D) INSTALLROOT=$(HOST_DIR) install
+#endef
+
+define SYSLINUX_INSTALL_TARGET_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) INSTALLROOT=$(TARGET_DIR) install
 endef
 
 $(eval $(generic-package))
